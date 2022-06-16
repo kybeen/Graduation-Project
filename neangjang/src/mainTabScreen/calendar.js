0509256fca66201cb0263a530fbb30ff4b683ac3
kybeen <rei980627@gmail.com>
2022-06-16 15:46:21 +0900

    v0.2.6

--- neangjang/src/mainTabScreen/calendar.js
+++ neangjang/src/mainTabScreen/calendar.js
@@ -14,15 +14,43 @@ import {
   Button,
   StyleSheet,
 } from 'react-native';
+import { Calendar, CalendarList } from 'react-native-calendars';
 
-const Calendar = () => {
+const CalendarView = () => {
   return (
-    <Text>Calendar</Text>
+    <View style={styles.container}>
+      <Calendar style={styles.calendar}
+        // Callback which gets executed when visible months change in scroll view. Default = undefined
+        onVisibleMonthsChange={(months) => {console.log('now these months are visible', months);}}
+        // Max amount of months allowed to scroll to the past. Default = 50
+        pastScrollRange={50}
+        // Max amount of months allowed to scroll to the future. Default = 50
+        futureScrollRange={50}
+        // Enable or disable scrolling of calendar list
+        scrollEnabled={true}
+        // Enable or disable vertical scroll indicator. Default = false
+        showScrollIndicator={true}
+      />
+      <View style={styles.about}>
+        <Text>해당 날짜 식품정보</Text>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+  },
+  calendar: {
+    felx: 1,
+  },
+  about: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'lightgray',
+  }
 });
 
-export default Calendar;
+export default CalendarView;
